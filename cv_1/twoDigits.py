97363a8a8847ed8c7d170771f0e13bc6ff20fda9
ThirstyMango <vojtaraja@seznam.cz>
2025-09-30 16:07:16 +0200

    Working version with very bad memory handling

--- cv_1/twoDigits.py
+++ cv_1/twoDigits.py
@@ -1,10 +1,5 @@
 import sys
 
-# 1. Exponent should be in a binary form
-# 2. Horners scheme should be used
-# 3. Watch out for 01
-
-
 # Digits are encoded between 48 and 57
 LOW_MARGIN_ASCII_DIG = 48
 TOP_MARGIN_ASCII_DIG = 57
@@ -12,40 +7,8 @@ TOP_MARGIN_ASCII_DIG = 57
 def toASCII(char:str) -> int:
     return ord(char)
 
-def toBase(decNum:int, base:int) -> list[int]:
-    currNum = decNum
-    baseList = []
-
-    while (currNum > 0):
-        remainder = currNum % base
-        currNum //= base
-        baseList.insert(0,remainder)
-
-    return baseList
-
 def isValidDigitASCII(charASCII:str) -> bool:
     return charASCII >= LOW_MARGIN_ASCII_DIG and charASCII <= TOP_MARGIN_ASCII_DIG
-
-def getNextChar() -> str:
-    nextChar = sys.stdin.read(1)
-    return nextChar
-
-def getNextNumber() -> int:
-    nextNumber = 0 # Assume for horners scheme
-
-    nextCharASCII = toASCII(getNextChar())
-
-    # Remove garbage
-    while (not isValidDigitASCII(nextCharASCII)):
-        nextCharASCII = toASCII(getNextChar())
-
-    # Is a valid digit [0-9]
-    while (isValidDigitASCII(nextCharASCII)):
-        currDigit = nextCharASCII - LOW_MARGIN_ASCII_DIG
-        nextNumber = nextNumber * 10 + currDigit
-        nextCharASCII = toASCII(getNextChar())
-    
-    return nextNumber
 
 def getLastNDigits(number:int, n:int) -> int:
     numberStr = str(number)
@@ -54,22 +17,45 @@ def getLastNDigits(number:int, n:int) -> int:
 
 def runDigitProgram():
 
-    baseNumber = getNextNumber()
-    power = getNextNumber()
+    def getNextChar() -> str:
+        nextChar = sys.stdin.read(1)
+        return nextChar
 
+    def getNextNumber() -> int:
+        nextNumber = 0 # Assume for horners scheme
+
+        nextCharASCII = toASCII(getNextChar())
+
+        # Remove garbage
+        while (not isValidDigitASCII(nextCharASCII)):
+            nextCharASCII = toASCII(getNextChar())
+
+        # Is a valid digit [0-9]
+        while (isValidDigitASCII(nextCharASCII)):
+            currDigit = nextCharASCII - LOW_MARGIN_ASCII_DIG
+            nextNumber = nextNumber * 10 + currDigit
+            nextCharASCII = toASCII(getNextChar())
+        
+        return nextNumber
+    
     def getLastTwoPowerDigits(baseNumber, power):
         baseLastTwo = cLastTwo = getLastNDigits(baseNumber, 2) # Only last two digits matter
-        powerInBiBase = toBase(power, 2) # Exponent will be in binary for concatenation via Horner's scheme
+        powerArray = [int(x) for x in str(power)]
 
-        for i in range(1, len(powerInBiBase)):
-            cDigit = powerInBiBase[i]
+        for i in range(1, len(powerArray)):
+            cDigit = powerArray[i]
 
             if (cDigit == 0):
                 cLastTwo = getLastNDigits(cLastTwo * cLastTwo, 2)
             elif (cDigit == 1):
                 cLastTwo = getLastNDigits(cLastTwo * cLastTwo * baseLastTwo, 2)
 
-        return cLastTwo
+        cLastTwoStr = str(cLastTwo) if len(str(cLastTwo)) >= 2 else "0" + str(cLastTwo)
+
+        return cLastTwoStr
+
+    baseNumber = getNextNumber()
+    power = getNextNumber()
 
     lastTwoPowerDigits = getLastTwoPowerDigits(baseNumber, power)
     print(lastTwoPowerDigits)
